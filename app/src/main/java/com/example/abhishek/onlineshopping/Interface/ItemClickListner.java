2527cbc2c22ba28b2cad3b6832fffd23fd44b86e
basu12 <basanagoudap9141@gmail.com>
2020-05-08 22:07:59 +0530

    first commit

---
+++ app/src/main/java/com/example/abhishek/onlineshopping/Interface/ItemClickListner.java
@@ -0,0 +1,12 @@
+package com.example.abhishek.onlineshopping.Interface;
+
+/**
+ * Created by abhishek on 10-04-2020.
+ */
+
+        import android.view.View;
+
+public interface ItemClickListner
+{
+    void onClick(View view, int position, boolean isLongClick);
+}
---
+++ app/src/main/java/com/example/abhishek/onlineshopping/Prevalent/Prevalent.java
@@ -0,0 +1,14 @@
+package com.example.abhishek.onlineshopping.Prevalent;
+
+/**
+ * Created by abhishek on 10-04-2020.
+ */
+import com.example.abhishek.onlineshopping.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+}
+
